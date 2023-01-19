9896047f6217ae010ab7dfa710633e2ed4ed6fcf
fingerskier <fingerskier@gmail.com>
2023-01-19 09:04:41 -0600

    ssl

--- cloud/app.js
+++ cloud/app.js
@@ -1,20 +1,97 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const debug = require('debug')('cloud:app')
+const express = require('express')
+const https = require('https')
+const pem = require('pem')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
 
-var app = express();
+let app
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+function normalizePort(val) {
+  var port = parseInt(val, 10);
 
-module.exports = app;
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+
+function onListening(server) {
+  // var addr = server.address();
+  // var bind = typeof addr === 'string'
+  //   ? 'pipe ' + addr
+  //   : 'port ' + addr.port;
+  // debug('Listening on ' + bind);
+  debug('Listening', server)
+}
+
+
+pem.createCertificate({days: 111, selfSigned: true}, (err,keys)=>{
+  const http = require('http')
+  
+  const port = normalizePort(process.env.PORT || '3210')
+  
+  const server = http.createServer(app)
+  
+  if (err) throw err
+  
+  app = express()
+
+  app.set('port', port)
+  
+  app.use(logger('dev'))
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
+  app.use(cookieParser())
+  app.use(express.static(path.join(__dirname, 'public')))
+  
+  app.use('/', indexRouter)
+  app.use('/users', usersRouter)
+
+
+  https.createServer({
+    key: keys.serviceKey,
+    cert: keys.certificate,
+  }, app)
+  .listen(port)
+  .on('error', onError)
+  .on('listening', onListening)
+})
--- edge/app.js
+++ edge/app.js
@@ -1,4 +1,7 @@
+const debug = require('debug')('cloud:app')
 const express = require('express')
+const https = require('https')
+const pem = require('pem')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
@@ -6,16 +9,89 @@ const logger = require('morgan')
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 
-const app = express()
-
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+let app
 
 
-module.exports = app
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+
+function onListening(server) {
+  // var addr = server.address();
+  // var bind = typeof addr === 'string'
+  //   ? 'pipe ' + addr
+  //   : 'port ' + addr.port;
+  // debug('Listening on ' + bind);
+  debug('Listening', server)
+}
+
+
+pem.createCertificate({days: 111, selfSigned: true}, (err,keys)=>{
+  const http = require('http')
+  
+  const port = normalizePort(process.env.PORT || '3456')
+  
+  const server = http.createServer(app)
+  
+  if (err) throw err
+  
+  app = express()
+
+  app.set('port', port)
+  
+  app.use(logger('dev'))
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
+  app.use(cookieParser())
+  app.use(express.static(path.join(__dirname, 'public')))
+  
+  app.use('/', indexRouter)
+  app.use('/users', usersRouter)
+
+
+  https.createServer({
+    key: keys.serviceKey,
+    cert: keys.certificate,
+  }, app)
+  .listen(port)
+  .on('error', onError)
+  .on('listening', onListening)
+})
